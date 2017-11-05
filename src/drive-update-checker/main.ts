34b3a9bd3490260190e1af4a99d597780b02a3b2
Yuichi Maekawa <kaelaela.31@gmail.com>
2017-11-06 02:12:16 +0900

    Enable to check multi files

--- src/drive-update-checker/main.ts
+++ src/drive-update-checker/main.ts
@@ -1,6 +1,6 @@
 const slackUrl = "YOUR_WEBHOOK_URL" //edit here
 const TARGET_FOLDER_ID = "YOUR_TARGET_FOLDER" //edit here
-const TARGET_FILE_NAME = "YOUR_FILE_NAME" //edit here
+const TARGET_FILES = ["YOUR", "TARGET", "FILE", "NAMES"] //edit here
 
 class SlackPayload {
   channel: string
@@ -9,49 +9,60 @@ class SlackPayload {
 }
 
 function createPayload(text: string) {
-  let payload: SlackPayload = {
+  const payload: SlackPayload = {
     "channel": "YOUR_CHANNEL", //edit here
     "username": "YOUR_NAME", //edit here
-    "text": text,
-  };
-  return payload;
+    "text": text
+  }
+  return payload
 }
 
 function postSlack(payload: SlackPayload) {
-  var payloadStr = JSON.stringify(payload);
-  var escapedStr = payloadStr.replace(/":"/g, "\"\:\"");
-  var options = {
+  const payloadStr = JSON.stringify(payload)
+  const escapedStr = payloadStr.replace(/":"/g, "\"\:\"")
+  const options = {
     'method': 'post',
-    'contentType': 'Content-type: application/json; charset=utf-8',
-    'payload': escapedStr,
-    'icon_emoji': ':monkey_face:'
+    'contentType': 'Content-type: application/json charset=utf-8',
+    'payload': escapedStr
   }
 
-  var response = UrlFetchApp.fetch(slackUrl, options) || false;
-  var ret = true;
-  if (!response || response.getResponseCode() != 200) {
-    ret = false;
-  }
-  return {status: ret, code: response.getResponseCode(), response: response};
+  const response = UrlFetchApp.fetch(slackUrl, options) || false
+  const ret = (response && response.getResponseCode() == 200)
+  return { status: ret, code: response.getResponseCode(), response: response }
 }
 
-function updateCheck() {
-  var file = DriveApp.getFilesByName(TARGET_FILE_NAME).next()
-  var lastUpdate: Date = file.getLastUpdated()
-  Logger.log("lastUpdate:"+ lastUpdate)
-  var cachedLastUpdate = CacheService.getScriptCache().get(TARGET_FILE_NAME)
-  Logger.log("cachedLastUpdate:"+ cachedLastUpdate)
-  if (cachedLastUpdate == null) {
-    CacheService.getScriptCache().put(TARGET_FILE_NAME, lastUpdate.toString())
-    Logger.log("saved")
-    return
-  } else if (new Date(cachedLastUpdate) < new Date(lastUpdate)) {
-    CacheService.getScriptCache().put(TARGET_FILE_NAME,lastUpdate.toString())
-    Logger.log("saved:"+ lastUpdate)
-  } else {
-    return
-  }
+function main() {
+  updateCheck(DriveApp.getFolderById(TARGET_FOLDER_ID))
+}
 
-  var updateText = "ファイルが更新されました" //edit here
-  postSlack(createPayload(updateText))
+function updateCheck(target: GoogleAppsScript.Drive.Folder) {
+  const files = target.getFiles()
+  while (files.hasNext()) {
+    const file = files.next()
+    const lastUpdate = file.getLastUpdated()
+    for (var i = 0, length = TARGET_FILES.length; i < length; i++) {
+      const fileName = TARGET_FILES[i]
+      if (fileName != file.getName()) {
+        continue
+      }
+      const cachedLastUpdate = CacheService.getScriptCache().get(fileName)
+      if (cachedLastUpdate == null) {
+        CacheService.getScriptCache().put(fileName, lastUpdate.toString())
+        continue
+      } else if (new Date(cachedLastUpdate) < new Date(lastUpdate)) {
+        CacheService.getScriptCache().put(fileName, lastUpdate.toString())
+      } else {
+        continue
+      }
+      const updateText = fileName + "was updated."
+      //変更者の名前が欲しい場合、gsファイルでここを有効にしてください
+      // var updateText = Drive.Files.get(file.getId())
+      //   .lastModifyingUser.displayName + "が" + fileName + "を更新しました"
+      postSlack(createPayload(updateText))
+    }
+  }
+  var folders = target.getFolders()
+  while (folders.hasNext()) {
+    updateCheck(folders.next())
+  }
 }
